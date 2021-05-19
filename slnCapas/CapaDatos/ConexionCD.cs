fe424ba779b4185bf8c60344500d58a73cbbadb6
carlocordova21 <57576613+carlocordova21@users.noreply.github.com>
2021-05-19 12:35:06 -0500

    Add files via upload

---
+++ slnCapas/CapaDatos/ConexionCD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+namespace CapaDatos
+{
+    class ConexionCD
+    {
+        public static SqlConnection conectarSqlServer()
+        {
+            SqlConnectionStringBuilder cadena = new SqlConnectionStringBuilder();
+            cadena.DataSource = "localhost";
+            cadena.InitialCatalog = "BDMARKET";
+            cadena.UserID = "sa";
+            cadena.Password = "12345";
+            string cadenaConexion = cadena.ConnectionString;
+
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            return conexion;
+        }
+    }
+}
